6be05cc821b32c5886f00e8896468e4c991e51bf
Yocheved <Rachelkupetz@gmail.com>
2025-06-24 13:17:23 +0300

    Update Bug class

--- GovernmentBug/src/Domain/Entities/Bug.cs
+++ GovernmentBug/src/Domain/Entities/Bug.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace GovernmentBug.Domain.Common
 {
-    internal class Bug
+    public class Bug : BaseEvent
     {
+        public int BugID { get; set; }
+
+        [StringLength(225)]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [StringLength(50)]
+        public string PriortyId { get; set; }
+
+        [StringLength(50)]
+        public string StatusId { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
     }
 }
