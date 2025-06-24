733bb91d49287be5f021ed374a3f6e7d665cc287
dvori-zlotnik <dvori7330@gmail.com>
2025-06-24 14:10:51 +0300

    add attachment table

---
+++ GovernmentBug/src/Domain/Entities/Attachment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+
+namespace GovernmentBug.Domain.Entities
+{
+    public class Attachment
+    {
+       
+        [Key]
+        public int AttachmentId { get; set; }
+        public int BugId { get; set; }
+        [MaxLength(255)]
+        public virtual Bug Bug { get; set; } = null!;
+        public string FileName { get; set; } = string.Empty;
+        [MaxLength(50)]
+        public string FileType { get; set; } = string.Empty;
+        [MaxLength(500)]
+        public string FilePath { get; set; } = string.Empty;
+
+    }
+}
