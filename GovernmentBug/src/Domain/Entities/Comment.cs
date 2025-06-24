861f12409c965712851c3c5e1d5d9eef846ab61f
dvori-zlotnik <dvori7330@gmail.com>
2025-06-24 13:22:07 +0300

    add comment table

--- GovernmentBug/src/Domain/Entities/Comment.cs
+++ GovernmentBug/src/Domain/Entities/Comment.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GovernmentBug.Domain.Entities
 {
-    class Comment
+    public class Comment:BaseAuditableEntity
     {
+
+        /// <summary>
+        /// Comment identifier (Primary Key)
+        /// </summary>
+        [Key]
+        public int CommentID { get; set; }
+        /// <summary>
+        /// Foreign key to the Bugs table
+        /// </summary>
+        public int BugID { get; set; }
+
+        /// <summary>
+        /// The content of the comment
+        /// </summary>
+        public string CommentText { get; set; } = string.Empty;
+
+        /// <summary>
+        /// User identifier who made the comment
+        /// </summary>
+        public int CommentedBy { get; set; }
+
+        /// <summary>
+        /// Date and time the comment was added
+        /// </summary>
+        public DateTime CommentDate { get; set; }
     }
 }
