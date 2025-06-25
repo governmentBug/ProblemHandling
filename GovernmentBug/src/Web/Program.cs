8029ff6c359b2178425e01e9223bf95754f30f02
leah <l0583280864@gmail>
2025-06-25 12:23:28 +0300

    התקנת האוטו מאפאר והגדרת הMappingProfile

--- GovernmentBug/src/Web/Program.cs
+++ GovernmentBug/src/Web/Program.cs
@@ -1,3 +1,4 @@
+using GovernmentBug.Application.Common.Mappings;
 using GovernmentBug.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,7 +21,7 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 app.UseHealthChecks("/health");
 app.UseHttpsRedirection();
 app.UseStaticFiles();
