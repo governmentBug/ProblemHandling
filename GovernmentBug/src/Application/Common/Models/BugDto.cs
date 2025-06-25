7ece9c4407f2311cf4fd2494030c976c7d3d06ba
developElisheva <elisheva2107@gmail.com>
2025-06-25 13:03:47 +0300

    add Dto

---
+++ GovernmentBug/src/Application/Common/Models/BugDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GovernmentBug.Application.Common.Models;
+public class BugDto
+{
+    public int BugID { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public string PriortyId { get; set; } = string.Empty;
+
+    public string StatusId { get; set; } = string.Empty;
+
+    public int CreatedBy { get; set; }
+
+    public string CreatedByUserFullName { get; set; } = string.Empty;
+
+    public DateTime CreatedDate { get; set; }
+
+    public List<CommentDto> Comments { get; set; } = new();
+}
--- GovernmentBug/src/Application/Common/Models/UesrDto.cs
+++ GovernmentBug/src/Application/Common/Models/UesrDto.cs
@@ -1,20 +1,16 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GovernmentBug.Domain.Entities;
-public class User : BaseAuditableEntity
+namespace GovernmentBug.Application.Common.Models;
+public class UserDto
 {
-    [Key]
     public int UserId { get; set; }
-    [StringLength(100)]
     public string FullName { get; set; } = string.Empty;
-    [StringLength(100)]
     public string Email { get; set; } = string.Empty;
-    [StringLength(50)]
     public string Role { get; set; } = string.Empty;
-    public ICollection<Bug> CreatedBugs { get; set; } = new List<Bug>();
+    public List<BugDto> CreatedBugs { get; set; } = new();
+
 }
