0cafe191fa0c1e654bd53f8b623b021a91163d9b
developElisheva <elisheva2107@gmail.com>
2025-06-26 12:09:10 +0300

    Angular project update

--- GovernmentBug/src/Web/ClientApp/src/app/app.module.ts
+++ GovernmentBug/src/Web/ClientApp/src/app/app.module.ts
@@ -7,32 +7,15 @@ import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@a
 import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { TodoComponent } from './todo/todo.component';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        NavMenuComponent,
-        HomeComponent,
-        CounterComponent,
-        FetchDataComponent,
-        TodoComponent
-    ],
     bootstrap: [AppComponent],
     imports: [
         BrowserModule,
         FormsModule,
         RouterModule.forRoot([
-            { path: '', component: HomeComponent, pathMatch: 'full' },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
-            { path: 'todo', component: TodoComponent }
         ]),
         BrowserAnimationsModule,
         ModalModule.forRoot()],
---
+++ GovernmentBug/src/Web/ClientApp/src/app/features/all-bugs/all-bugs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-bugs',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-bugs.component.html',
+  styleUrl: './all-bugs.component.css'
+})
+export class AllBugsComponent {
+
+}
