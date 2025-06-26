b9fd7a7983eb5c3129687431c63f830d7ea0de86
dvori-zlotnik <dvori7330@gmail.com>
2025-06-26 12:53:35 +0300

    הוספת פונקצית שליפת כל הבאגים

---
+++ GovernmentBug/src/Web/Endpoints/Bugs.cs
@@ -0,0 +1,22 @@
+﻿using GovernmentBug.Application.Bugs.Queries.GetBugs;
+using GovernmentBug.Application.Common.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace GovernmentBug.Web.Endpoints;
+
+public class Bugs : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            .RequireAuthorization()
+            .MapGet(GetBugs);
+    }
+
+    public async Task<Ok<List<BugDto>>> GetBugs(ISender sender)
+    {
+        var result = await sender.Send(new GetBugsQuery());
+
+        return TypedResults.Ok(result);
+    }
+}
