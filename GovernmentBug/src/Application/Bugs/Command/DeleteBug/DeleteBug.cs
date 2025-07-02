73a34be701a89bb1f2a84c1b94211209ddf396ae
leah <l0583280864@gmail>
2025-07-02 14:07:38 +0300

    מחיקת באג

--- GovernmentBug/src/Application/Bugs/Command/DeleteBug/DeleteBug.cs
+++ GovernmentBug/src/Application/Bugs/Command/DeleteBug/DeleteBug.cs
@@ -3,8 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GovernmentBug.Application.Common.Interfaces;
 
 namespace GovernmentBug.Application.Bugs.Command.DeleteBug;
-internal class DeleteBug
 {
+    public record DeleteBugCommand(int BugId) : IRequest;
+
+    public class DeleteBugCommandHandler : IRequestHandler<DeleteBugCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteBugCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(DeleteBugCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Bugs
+                .FindAsync(new object[] { request.BugId }, cancellationToken);
+
+            Guard.Against.NotFound(request.BugId, entity);
+
+            // מחיקת הבאג מה־DbSet
+            _context.Bugs.Remove(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
 }
+
