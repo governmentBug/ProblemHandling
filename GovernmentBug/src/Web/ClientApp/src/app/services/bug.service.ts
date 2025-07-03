fc04ab9191413c98fb54c8cb4c2037b9a240aa01
leah <l0583280864@gmail>
2025-07-03 15:52:58 +0300

    fixed serviceBug

--- GovernmentBug/src/Web/ClientApp/src/app/services/bug.service.ts
+++ GovernmentBug/src/Web/ClientApp/src/app/services/bug.service.ts
@@ -7,21 +7,21 @@ import { Bug } from '../models/bug.model';
   providedIn: 'root'
 })
 export class BugService {
-  constructor(public bugServer: HttpClient) {}
-
+  constructor(public bugServer: HttpClient) { }
+  selectedBug :Bug = {
+    bugID: 1,
+    title: 'שגיאה בטעינת דף הבית',
+    description: 'בעת כניסה לדף הבית מתקבלת שגיאת 500',
+    priortyId: '1',
+    statusId: '1',
+    createdByUserId:5,
+    createdByUserFullName: 'רונית כהן',
+    createdDate: new Date('2025-06-25T09:15:00'),
+    comments: [
+    ]
+  }
   allBugs: Array<Bug> = new Array<Bug>();
-selectedBug:Bug={
-   bugID: 1,
-  title: 'שגיאה בטעינת דף הבית',
-  description: 'בעת כניסה לדף הבית מתקבלת שגיאת 500',
-  priortyId: '1',
-  statusId: '1',
-  createdByUserId: 101,
-  createdByUserFullName: 'רונית כהן',
-  createdDate: new Date('2025-06-25T09:15:00'),
-  comments: [
-  ],
-}
+
   basicUrl: string = "https://localhost:5001/api/Bugs/";
 
   getAllBugs(): Observable<Array<Bug>> {
