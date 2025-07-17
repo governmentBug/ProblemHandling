276165bcf1b4d2a30b8c9f52a80c273717d2e239
Yocheved <Rachelkupetz@gmail.com>
2025-07-17 12:28:34 +0300

    add all

---
+++ GovernmentBug/src/Web/ClientApp/src/app/features/all-attachments/all-attachments.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-attachments',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-attachments.component.html',
+  styleUrl: './all-attachments.component.css'
+})
+export class AllAttachmentsComponent {
+
+}
--- GovernmentBug/src/Web/ClientApp/src/main.ts
+++ GovernmentBug/src/Web/ClientApp/src/main.ts
@@ -5,21 +5,25 @@ import { environment } from './environments/environment';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { routes } from './app/app.routes';
+//import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export function getBaseUrl() {
   return document.getElementsByTagName('base')[0].href;
 }
 
+
+
 const providers = [
   { provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] },
   provideHttpClient(withInterceptorsFromDi()),
-  importProvidersFrom(RouterModule.forRoot(routes))
+  importProvidersFrom(RouterModule.forRoot(routes)),
+  //provideAnimationsAsync() // הוסף כאן
 ];
 
 if (environment.production) {
   enableProdMode();
 }
-
 bootstrapApplication(AppComponent, {
   providers: providers
 }).catch(err => console.error(err));
+
